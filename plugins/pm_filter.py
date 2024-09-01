2deaab091ea213e5ba8dfe864fbc25bbdcd51be1
ST Thamizhan <130844687+ewoneST@users.noreply.github.com>
2024-09-01 19:27:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -186,9 +186,6 @@ async def next_page(bot, query):
                 off_set = offset - int(MAX_B_TN)
             if n_offset == 0:
                 btn.append(
-                    [InlineKeyboardButton(" 🪅 ʟᴀɴɢᴜᴀɢᴇs 🪅 ", callback_data=f"languages#{key}")]
-                )
-                btn.append(
                     [InlineKeyboardButton("⌫ 𝐁𝐀𝐂𝐊", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"{math.ceil(int(offset)/int(MAX_B_TN))+1} / {math.ceil(total/int(MAX_B_TN))}", callback_data="pages")]
                 )
             elif off_set is None:
@@ -211,12 +208,10 @@ async def next_page(bot, query):
             off_set = offset - 10
         if n_offset == 0:
             btn.append(
-                    [InlineKeyboardButton(" 🪅 ʟᴀɴɢᴜᴀɢᴇs 🪅 ", callback_data=f"languages#{key}")]
-            )
-            btn.append(
                 [InlineKeyboardButton("⌫ 𝐁𝐀𝐂𝐊", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"{math.ceil(int(offset)/10)+1} / {math.ceil(total/10)}", callback_data="pages")]
             )
         elif off_set is None:
+            btn.append([InlineKeyboardButton(" 🪅 ʟᴀɴɢᴜᴀɢᴇs 🪅 ", callback_data=f"languages#{key}")])
             btn.append([InlineKeyboardButton("𝐏𝐀𝐆𝐄", callback_data="pages"), InlineKeyboardButton(f"{math.ceil(int(offset)/10)+1} / {math.ceil(total/10)}", callback_data="pages"), InlineKeyboardButton("𝐍𝐄𝐗𝐓 ➪", callback_data=f"next_{req}_{key}_{n_offset}")])
         else:
             btn.append(
